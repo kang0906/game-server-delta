a47cc40f3ab05d5f13d7e56aaab4500f6d8d4080
jinkoo kang <jinkoo906@gmail.com>
2026-03-13 10:52:32 +0900

    #8 예외가 발생하면 alert 를 전송 하도록 GlobalExceptionHandler 수정

--- game/src/main/java/com/example/game/common/exception/GlobalExceptionHandler.java
+++ game/src/main/java/com/example/game/common/exception/GlobalExceptionHandler.java
@@ -1,14 +1,26 @@
 package com.example.game.common.exception;
 
+import com.example.game.admin.alert.entity.AlertLevel;
+import com.example.game.admin.alert.service.AlertService;
 import com.example.game.common.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+    private final AlertService alertService;
+
     @ExceptionHandler(GlobalException.class)
     protected ResponseEntity<?> handleGlobalException(GlobalException e) {
+
+        if(e.getErrorCode() == ErrorCode.CANNOT_OVERLAP_UNIT){
+            alertService.sendAlert(AlertLevel.WARN, e.getMessage());
+        }
 
         return ResponseEntity.status(e.getErrorCode().getHttpStatus())
                 .body(ResponseDto.fail(
